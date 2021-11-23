9596b78c21b44731a37bffea676519bca8671c3d
ahua <ahua1314251@163.com>
2021-11-24 00:24:59 +0800

    update 发布脚本

--- deploy63.sh
+++ deploy63.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/expect
 set password [lindex $argv 0]
 set timeout -1
-exec cp ./target/winweb.war ./target/ROOT.war
-spawn scp -o StrictHostKeyChecking=no  -P 22 -r ./target/ROOT.war admin@106.54.255.63:/home/admin/apache-tomcat-8.5.47
+exec cp ./target/winblog.war ./target/ROOT.war
+spawn scp -o StrictHostKeyChecking=no  -P 22 -r ./target/ROOT.war admin@182.254.138.250:/home/admin/apache-tomcat-8.5.35
 expect {
  "(yes/no)?" {
    send "yes\n"
@@ -15,11 +15,11 @@ expect {
 expect eof
 
 #!/usr/bin/expect
-spawn ssh admin@106.54.255.63
+spawn ssh admin@182.254.138.250
 expect {
         "*yes/no" { send "yes\r"; exp_continue }
         "*password:" { send "$password\r"; exp_continue }
-        "]*" {send "rm -rf /home/admin/apache-tomcat-8.5.47/webapps/ROOT && cp /home/admin/apache-tomcat-8.5.47/ROOT.war  /home/admin/apache-tomcat-8.5.47/webapps/ROOT.war\r" ;}
+        "]*" {send "rm -rf /home/admin/apache-tomcat-8.5.47/webapps/ROOT && cp /home/admin/apache-tomcat-8.5.35/ROOT.war  /home/admin/apache-tomcat-8.5.35/webapps/ROOT.war\r" ;}
 
     }
 
